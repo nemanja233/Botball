d176bfbf566a871cc0f5e7d27f4b3d41ccfdab30
sunrise656 <julian.regensburger963@gmail.com>
2026-03-11 16:50:54 +0100

    cone dispenser ready und drive down für omni

--- drive_down_omni.py
+++ drive_down_omni.py
@@ -3,6 +3,12 @@ def motors(sets):
     for count, motor in enumerate(sets):
         k.motor(count, motor)
 
+def turn_90():
+    for i in range(2):
+        motors([100,-100,100,-100])
+        k.msleep(730)
+    k.ao()
+
 def line_follower():
     while k.analog(0) >= 300:
         sens_r = int(str(k.analog(2))[:-1] + "0")
@@ -26,6 +32,22 @@ def on_touch():
 
 line_follower()
 on_touch()
+
+for i in range(1):
+    motors([-100,-100,-100,-100])
+    k.msleep(1000)
+
+turn_90()
+
+for i in range(1):
+    motors([-100,-100,-100,-100])
+    k.msleep(1000)
+
+
+  
+    
+    
+
 
   
     
